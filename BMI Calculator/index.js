4f52e8028ed9946bae9a5e97ed6aeae0ef1b4f61
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-04-14 15:17:22 +0530

    Add files via upload

---
+++ BMI Calculator/index.js
@@ -0,0 +1,22 @@
+console.log("BMI");
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const result = document.querySelector('#result');
+
+  if((height === '') || (height < 0) || (isNaN(height)) ){
+      result.innerHTML = "Please provide a valid height";
+  }
+  else if (weight === '' || weight < 0 || isNaN(weight)){
+     result.innerHTML = "Please provide a valid weight";
+  }
+  else {
+     const bmi = (weight / ((height*height)/10000)).toFixed(2);
+     result.innerHTML = `BMI is : <span>${bmi}</span>`;
+  }
+
+});
